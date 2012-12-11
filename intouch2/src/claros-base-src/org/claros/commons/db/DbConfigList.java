45bf299642309f76481dee2750df55890e467eeb
Luis Ashurei <atealxt@gmail.com>
2012-12-11 01:01:32 -0800

    Try to use spring DBCP instead of itouch DBCP

--- intouch2/src/claros-base-src/org/claros/commons/db/DbConfigList.java
+++ intouch2/src/claros-base-src/org/claros/commons/db/DbConfigList.java
@@ -7,11 +7,13 @@ import javax.sql.DataSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.zhyfoundry.crm.core.DIManager;
+
 public class DbConfigList {
     private static Log log = LogFactory.getLog(DbConfigList.class);
-    
+
     public static HashMap dbList = new HashMap();
-	
+
 	public void addDbConfig(DbConfig db) {
 		dbList.put(db.getId(), db);
 	}
@@ -19,14 +21,21 @@ public class DbConfigList {
 	public static HashMap getDbList() {
 		return dbList;
 	}
-	
+
+	// TODO test
+	public static DataSource getDataSource() {
+		return DIManager.getBean(org.apache.commons.dbcp.BasicDataSource.class);
+	}
+
+	@Deprecated
 	public static DataSource getDataSourceById(String id) {
-	    DbConfig db = (DbConfig)dbList.get(id);
-	    if (db != null) {
-		    return db.getDataSource();
-	    } else {
-	        log.warn("The Id Searched at the DbConfigList does not correspond to a DbConfig");
-	        return null;
-	    }
+//	    DbConfig db = (DbConfig)dbList.get(id);
+//	    if (db != null) {
+//		    return db.getDataSource();
+//	    } else {
+//	        log.warn("The Id Searched at the DbConfigList does not correspond to a DbConfig");
+//	        return null;
+//	    }
+		return getDataSource();
 	}
 }

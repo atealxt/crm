2fa5cb7c49c07b0f8db2967a575fefddeb18edef
Luis Ashurei <atealxt@gmail.com>
2013-01-04 22:10:07 -0800

    Add timestamp to log

--- src/main/webapp/javascripts/admin/mail.js
+++ src/main/webapp/javascripts/admin/mail.js
@@ -1,11 +1,16 @@
 var connection;
 $(document).ready(function() {
     if (window.WebSocket) {
-        if (typeof String.prototype.startsWith != 'function') {
-            String.prototype.startsWith = function (str){
-                return this.indexOf(str) == 0;
-            };
-        }
+    	function js_yyyy_mm_dd_hh_mm_ss() {
+    		var now = new Date();
+    		year = "" + now.getFullYear();
+    		month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
+    		day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
+    		hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+    		minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+    		second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
+    		return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+    	}
         var sent = 0;
         var success = 0;
         connection = new WebSocket(baseWebSocketPath + '/webmail/sendMailWebSocket.cl');
@@ -23,7 +28,7 @@ $(document).ready(function() {
                 $('#dynamicErrorMessage').text(e.data);
                 return;
             } else if (e.data.indexOf('log:') == 0) {
-                $('#tbodyMailLog').append('<tr><td>' + e.data.substring(4) + '</td></tr>');
+                $('#tbodyMailLog').append('<tr><td>' + js_yyyy_mm_dd_hh_mm_ss() + ' ' + e.data.substring(4) + '</td></tr>');
                 return;
             }
             var data = JSON.parse(e.data);

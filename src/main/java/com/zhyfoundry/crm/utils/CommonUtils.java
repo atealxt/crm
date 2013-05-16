822a83b498fed7767ea15b8370428f1475843d08
Luis Ashurei <atealxt@gmail.com>
2013-05-16 05:25:36 -0700

    Upgrade to 1.2.3: support input specified page number.

--- src/main/java/com/zhyfoundry/crm/utils/CommonUtils.java
+++ src/main/java/com/zhyfoundry/crm/utils/CommonUtils.java
@@ -8,31 +8,34 @@ import org.springframework.util.DigestUtils;
 
 public final class CommonUtils {
 
-	private CommonUtils() {
-	}
+    private CommonUtils() {
+    }
 
-	public static int String2Int(final String str, final int defaultValue) {
-		if (StringUtils.isEmpty(str)) {
-			return defaultValue;
-		}
-		return Integer.parseInt(str);
-	}
+    public static int String2Int(final String str, final int defaultValue) {
+        if (StringUtils.isEmpty(str)) {
+            return defaultValue;
+        }
+        if (StringUtils.isNumeric(str)) {
+            return Integer.parseInt(str);
+        }
+        return defaultValue;
+    }
 
-	public static String md5Hex(final String data) throws NoSuchAlgorithmException {
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		return DigestUtils.md5DigestAsHex(md.digest(data.getBytes()));
-	}
+    public static String md5Hex(final String data) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        return DigestUtils.md5DigestAsHex(md.digest(data.getBytes()));
+    }
 
-	public static String brief(String source, int length) {
-		if (source == null) {
-			return source;
-		}
-		if (StringUtils.isEmpty(source)) {
-			return source;
-		}
-		if (source.length() < length) {
-			return source;
-		}
-		return source.substring(0, length) + "...";
-	}
+    public static String brief(String source, int length) {
+        if (source == null) {
+            return source;
+        }
+        if (StringUtils.isEmpty(source)) {
+            return source;
+        }
+        if (source.length() < length) {
+            return source;
+        }
+        return source.substring(0, length) + "...";
+    }
 }
